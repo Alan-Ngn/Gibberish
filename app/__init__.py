00e870a6a6cb17d0ab11a07f18cfb067dbd6c34c
Alan Nguyen <alanngn21@gmail.com>
2023-05-17 12:36:55 -0700

    organizing folders

--- app/__init__.py
+++ app/__init__.py
@@ -5,8 +5,7 @@ from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
 from flask_login import LoginManager
 from .models import db, User, Channel
-from .api.user_routes import user_routes
-from .api.auth_routes import auth_routes
+from .api import auth_routes, user_routes
 from .seeds import seed_commands
 from .config import Config
 
